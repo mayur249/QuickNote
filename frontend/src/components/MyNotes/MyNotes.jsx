6b5a22ec541c194cc4a26671f3c665197be5b7b2
mayur249 <kundermayuresh249@gmail.com>
2021-10-06 18:42:07 +0530

    created mynotes component

---
+++ frontend/src/components/MyNotes/MyNotes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainScreen } from "../";
+
+const MyNotes = () => {
+  return (
+    <div>
+      <MainScreen title="Welcome back Mayuresh">My Notes</MainScreen>
+    </div>
+  );
+};
+
+export default MyNotes;
