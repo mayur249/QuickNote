79ddd45543657d7ef09433ad521e32e0151f7566
mayur249 <kundermayuresh249@gmail.com>
2021-10-06 13:06:36 +0530

    changed font color of links and title

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Container,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/" style={{ color: "#ffffff" }}>
+          QuickNote
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="m-auto">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="mr-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav
+            className="my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action2" style={{ color: "#ffffff" }}>
+              My Notes
+            </Nav.Link>
+            <NavDropdown
+              title={<span style={{ color: "#ffffff" }}>Mayuresh Kunder</span>}
+              id="navbarScrollingDropdown"
+              style={{ color: "#ffffff" }}
+            >
+              <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
