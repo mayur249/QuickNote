e48b78df8e475212adda318a28fc6627f1ccc88a
mayur249 <kundermayuresh249@gmail.com>
2021-10-06 18:44:24 +0530

    added main screen and mynotes component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
 export { default as LandingPage } from "./LandingPage/LandingPage";
+export { default as MyNotes } from "./MyNotes/MyNotes";
+export { default as MainScreen } from "./MainScreen/MainScreen";
