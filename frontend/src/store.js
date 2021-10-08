e85f00e914e2b06ff7fd0597697d65b3acc1e951
mayur249 <kundermayuresh249@gmail.com>
2021-10-08 21:23:18 +0530

    started creating a store

---
+++ frontend/src/store.js
@@ -0,0 +1,6 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+  // this will contain our reducers
+});
