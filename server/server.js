9068b056334e50eaf14f8fecd9adde9b7558438b
mayur249 <kundermayuresh249@gmail.com>
2021-10-08 12:34:05 +0530

    added routes to server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,17 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
 
+//My Routes
+const userRoutes = require("./routes/userRoutes");
+
+// DB Connection
 connectDB();
+
+//middleware
+app.use(express.json());
+
+//My Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello There");
