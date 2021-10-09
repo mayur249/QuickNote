3757b6633cb0d84c6c33b1ba7f62223fd7411c7e
mayur249 <kundermayuresh249@gmail.com>
2021-10-09 09:51:44 +0530

    created store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,25 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { userSigninReducer, userSignupReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
-  // this will contain our reducers
+  userSignin: userSigninReducer,
+  userSignup: userSignupReducer,
 });
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = { userSignin: { userInfo: userInfoFromStorage } };
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
